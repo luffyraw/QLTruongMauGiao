3f06ecf56f2345d91eb6cbbdcf8370950f9240f7
TranVanThin <92080765+luffyraw@users.noreply.github.com>
2021-12-15 15:51:40 +0700

    update

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyTruongMauGiao.Models;
+
+
+namespace QuanLyTruongMauGiao.Controllers
+{
+    public class LoginController : Controller
+    {
+        private QLMauGiao db = new QLMauGiao();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
--- Models/THUCDONTUAN.cs
+++ Models/THUCDONTUAN.cs
@@ -1,10 +1,11 @@
-namespace QuanLyTruongMauGiao.Models
+﻿namespace QuanLyTruongMauGiao.Models
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.ComponentModel;
 
     [Table("THUCDONTUAN")]
     public partial class THUCDONTUAN
@@ -16,12 +17,15 @@ namespace QuanLyTruongMauGiao.Models
         }
 
         [Key]
+        [Required(ErrorMessage = "Không được để trống")]
         [StringLength(10)]
         public string MaTDT { get; set; }
 
+        [Required(ErrorMessage = "Không được để trống")]
         [Column(TypeName = "date")]
         public DateTime NgayBD { get; set; }
 
+        [Required(ErrorMessage = "Không được để trống")]
         [Column(TypeName = "date")]
         public DateTime NgayKT { get; set; }
 
